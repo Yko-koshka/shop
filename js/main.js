e3409e14f0860eb52983a2c77d1cd636127f5410
Hanna <legezaanna7@gmail.com>
2020-08-08 19:47:57 +0300

    add burger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+// scroll
+
+const header = document.querySelector(".header");
+
+function scroll() {
+  window.onscroll = () => {
+    if (window.pageYOffset > 50) {
+      header.classList.add("header_active");
+    } else {
+      header.classList.remove("header_active");
+    }
+  };
+}
+
+scroll();
+
+// anchors
+
+let anchors = document.querySelectorAll('header a[href*="#"]');
+
+for (anchor of anchors) {
+  if (anchor) {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      anchorId = this.getAttribute("href");
+      document.querySelector(anchorId).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
+}
+
 // Tabs
 
 document.querySelectorAll(".discounts-inner__item").forEach((item) =>
@@ -21,3 +54,20 @@ document.querySelectorAll(".discounts-inner__item").forEach((item) =>
 );
 
 document.querySelector(".discounts-inner__item").click();
+
+// Burger
+
+const burgerItem = document.querySelector(".burger");
+const menu = document.querySelector(".header-nav");
+const menuCloseItem = document.querySelector(".header-nav__close");
+
+function burger() {
+  burgerItem.addEventListener("click", () => {
+    menu.classList.add("header-nav_active");
+  });
+  menuCloseItem.addEventListener("click", () => {
+    menu.classList.remove("header-nav_active");
+  });
+}
+
+burger();
